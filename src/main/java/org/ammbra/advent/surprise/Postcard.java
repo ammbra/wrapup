2a70faaa2af0ac892dcb14ae9e1d649c6e2bdad0
Ana Maria Mihalceanu <mihalceanuana@gmail.com>
2023-12-03 09:40:30 +0100

    Update Postcard.java

--- src/main/java/org/ammbra/advent/surprise/Postcard.java
+++ src/main/java/org/ammbra/advent/surprise/Postcard.java
@@ -10,7 +10,7 @@ public record Postcard(String sender, String receiver, Celebration celebration) 
 					"sender": "\{sender}",
 				    "receiver": "\{receiver}",
 				    "message": "\{celebration.getText()}"
-				};
+				}
 				""" ;
 	}
 
