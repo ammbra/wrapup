7f801444e2b5c7ef5e962be7bbc4d4ee13ce7dbd
Ralf Spöth <ralf.spoeth@profidata.com>
2024-01-22 15:51:10 +0100

    ...made Wrapup public...

--- src/main/java/org/ammbra/advent/Wrapup.java
+++ src/main/java/org/ammbra/advent/Wrapup.java
@@ -1,7 +1,6 @@
 package org.ammbra.advent;
 
 import com.sun.net.httpserver.HttpExchange;
-import com.sun.net.httpserver.HttpHandler;
 import com.sun.net.httpserver.HttpServer;
 import io.github.ralfspoeth.json.Element;
 import io.github.ralfspoeth.json.JsonObject;
@@ -22,73 +21,69 @@ import java.util.Currency;
 import java.util.concurrent.Executors;
 
 
-class Wrapup implements HttpHandler {
+public class Wrapup {
+    public void main() throws IOException {
+        var server = HttpServer.create(
+                new InetSocketAddress("", 8081), 0);
+        var address = server.getAddress();
+        server.createContext("/", this::handle);
+        server.setExecutor(Executors.newVirtualThreadPerTaskExecutor());
+        server.start();
+        System.out.printf("http://%s:%d%n", address.getHostString(), address.getPort());
+    }
 
-	void main() throws IOException {
-		var server = HttpServer.create(
-				new InetSocketAddress("", 8081), 0);
-		var address = server.getAddress();
-		server.createContext("/", new Wrapup());
-		server.setExecutor(Executors.newVirtualThreadPerTaskExecutor());
-		server.start();
-		System.out.printf("http://%s:%d%n",address.getHostString(), address.getPort());
-	}
 
+    private void handle(HttpExchange exchange) throws IOException {
+        int statusCode = 200;
 
-	@Override
-	public void handle(HttpExchange exchange) throws IOException {
-		int statusCode = 200;
+        if (!"POST".equalsIgnoreCase(exchange.getRequestMethod())) {
+            statusCode = 400;
+        }
 
-		if (!"POST".equalsIgnoreCase(exchange.getRequestMethod())) {
-			statusCode = 400;
-		}
+        // Get the request body input stream
+        try (var rdr = new JsonReader(new InputStreamReader(exchange.getRequestBody()));
+             var wrtr = JsonWriter.createDefaultWriter(new OutputStreamWriter(exchange.getResponseBody()))
+        ) {
+            var req = (JsonObject) rdr.readElement();
+            var data = RequestConverter.convert(req);
 
-		// Get the request body input stream
-		try(var rdr = new JsonReader(new InputStreamReader(exchange.getRequestBody()));
-			var wrtr = JsonWriter.createDefaultWriter(new OutputStreamWriter(exchange.getResponseBody()))
-		)
-		{
-			var req = (JsonObject)rdr.readElement();
-			var data = RequestConverter.convert(req);
+            Postcard postcard = new Postcard(data.sender(), data.receiver(), data.celebration());
+            Intention intention = extractIntention(data);
+            var json = process(postcard, intention, data.choice());
 
-			Postcard postcard = new Postcard(data.sender(), data.receiver(), data.celebration());
-			Intention intention = extractIntention(data);
-			var json = process(postcard, intention, data.choice());
+            exchange.sendResponseHeaders(statusCode, 0);
 
-			exchange.sendResponseHeaders(statusCode, 0);
+            wrtr.write(json);
+        }
+    }
 
-			wrtr.write(json);
-		}
-	}
+    Intention extractIntention(RequestData data) {
+        return switch (data.choice()) {
+            case NONE -> new Coupon(0.0, null, Currency.getInstance("USD"));
+            case COUPON -> {
+                LocalDate localDate = LocalDateTime.now().plusYears(1).toLocalDate();
+                yield new Coupon(data.itemPrice(), localDate, Currency.getInstance("USD"));
+            }
+            case EXPERIENCE -> new Experience(data.itemPrice(), Currency.getInstance("EUR"));
+            case PRESENT -> new Present(data.itemPrice(), data.boxPrice(), Currency.getInstance("RON"));
+        };
+    }
 
-	Intention extractIntention(RequestData data) {
-		return switch (data.choice()) {
-			case NONE -> new Coupon(0.0, null, Currency.getInstance("USD"));
-			case COUPON -> {
-				LocalDate localDate = LocalDateTime.now().plusYears(1).toLocalDate();
-				yield new Coupon(data.itemPrice(), localDate, Currency.getInstance("USD"));
-			}
-			case EXPERIENCE -> new Experience(data.itemPrice(), Currency.getInstance("EUR"));
-			case PRESENT -> new Present(data.itemPrice(), data.boxPrice(), Currency.getInstance("RON"));
-		};
-	}
+    Element process(Postcard postcard, Intention intention, Choice choice) {
+        Gift gift = new Gift(postcard, intention);
 
-	Element process(Postcard postcard, Intention intention, Choice choice) {
-		Gift gift = new Gift(postcard, intention);
-
-		return switch (gift) {
-			case Gift(Postcard _, Postcard _) -> {
-				String message = "You cannot send two postcards!";
-				throw new UnsupportedOperationException(message);
-			}
-			case Gift(Postcard p, Coupon c)
-					when (c.price() == 0.0) -> p.toJsonObject();
-			case Gift(_, Coupon _), Gift(_, Experience _),
-					Gift(_, Present _) -> {
-				String option = choice.name().toLowerCase();
-				yield gift.merge(option);
-			}
-		};
-	}
+        return switch (gift) {
+            case Gift(Postcard _, Postcard _) -> {
+                String message = "You cannot send two postcards!";
+                throw new UnsupportedOperationException(message);
+            }
+            case Gift(Postcard p, Coupon c)
+                    when (c.price() == 0.0) -> p.toJsonObject();
+            case Gift(_, Coupon _), Gift(_, Experience _),
+                    Gift(_, Present _) -> {
+                String option = choice.name().toLowerCase();
+                yield gift.merge(option);
+            }
+        };
+    }
 }
-
