6bb6fcd934c2f74709bb0694230d1154ef975011
Ana <mihalceanuana@gmail.com>
2024-02-05 13:53:04 +0100

    Update JSON_VALIDATE expression

--- src/main/java/org/ammbra/advent/surprise/Intention.java
+++ src/main/java/org/ammbra/advent/surprise/Intention.java
@@ -14,24 +14,28 @@ public sealed interface Intention
 	StringTemplate.Processor<JSONObject, JSONException> JSON_VALIDATE =
 			(StringTemplate st) -> {
 				String quote = "\"";
-				List<Object> filtered = new ArrayList<>();
-				for (Object value : st.values()) {
-					if (value instanceof String str) {
-						if (str.contains(quote)) {
-							throw new JSONException("Injection vulnerability");
-						}
-						filtered.add(str);
-					} else if (value instanceof Number || value instanceof Boolean ||
-							value instanceof Currency || value instanceof LocalDate) {
-						filtered.add(value);
-					} else {
-						throw new JSONException("Invalid value type");
-					}
-				}
-				String jsonSource =
-						StringTemplate.interpolate(st.fragments(), filtered);
+				List<Object> filtered = validate(st, quote);
+				String jsonSource = StringTemplate.interpolate(st.fragments(), filtered);
 				return new JSONObject(jsonSource);
 			};
+
+	private static List<Object> validate(StringTemplate st, String quote) {
+		List<Object> filtered = new ArrayList<>();
+		for (Object value : st.values()) {
+			if (value instanceof String str) {
+				if (str.contains(quote)) {
+					throw new JSONException("Injection vulnerability");
+				}
+				filtered.add(str);
+			} else if (value instanceof Number || value instanceof Boolean ||
+					value instanceof Currency || value instanceof LocalDate) {
+				filtered.add(value);
+			} else {
+				throw new JSONException("Invalid value type");
+			}
+		}
+		return filtered;
+	}
 
 	JSONObject asJSON();
 }
